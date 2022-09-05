54269c84dcc5a8f2efd02884f37e2aa5bb0665d1
Bethelhem124 <59117329+Bethelhem124@users.noreply.github.com>
2022-09-05 16:03:27 +0300

    Update empleave.php

--- empleave.php
+++ empleave.php
@@ -14,14 +14,14 @@ $result = mysqli_query($conn, $sql);
 
 <html>
 <head>
-	<title>Employee Leave | Admin Panel | XYZ Corporation</title>
+	<title>Employee Leave | Admin Panel | ABUGIDA Corporation</title>
 	<link rel="stylesheet" type="text/css" href="styleview.css">
 </head>
 <body>
 	
 	<header>
 		<nav>
-			<h1>XYZ Corp.</h1>
+			<h1>ABUGIDA Corp.</h1>
 			<ul id="navli">
 				<li><a class="homeblack" href="aloginwel.php">HOME</a></li>
 				
