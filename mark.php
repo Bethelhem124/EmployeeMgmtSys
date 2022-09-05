13c0b3848fe176a2222295469b41da0df7eb4111
Bethelhem124 <59117329+Bethelhem124@users.noreply.github.com>
2022-09-05 16:05:30 +0300

    Update mark.php

--- mark.php
+++ mark.php
@@ -74,7 +74,7 @@ if(isset($_POST['update']))
 
 <html>
 <head>
-  <title>Project Mark | XYZ Corporation</title>
+  <title>Project Mark | ABUGIDA Corporation</title>
   <!-- Icons font CSS-->
     <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
     <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
@@ -91,7 +91,7 @@ if(isset($_POST['update']))
 <body>
   <header>
     <nav>
-      <h1>XYZ Corp.</h1>
+      <h1>ABUGIDA Corp.</h1>
       <ul id="navli">
         <li><a class="homeblack" href="aloginwel.php">HOME</a></li>
         <li><a class="homeblack" href="addemp.php">Add Employee</a></li>
